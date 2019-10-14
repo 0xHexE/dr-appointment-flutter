70858a76d52fe19ac90decb121d6d550d1745124
void_ptr_anmol <anmolkarmalkar@gmail.com>
2019-10-14 08:22:38 +0530

    added code

--- lib/model/notification_model.dart
+++ lib/model/notification_model.dart
@@ -1,17 +1,21 @@
 import 'dart:convert';
 
+import 'package:flutter_native_timezone/generated/i18n.dart';
+
 NotificationModel notificationModelFromJson(String str) {
   final jsonData = jsonDecode(str);
   return NotificationModel.fromJson(jsonData);
 }
 
 class NotificationModel {
+  List<String> key;
   List<String> description;
   List<String> name;
   List<DateTime> date;
   List<DateTime> time;
 
   NotificationModel({
+    this.key,
     this.description,
     this.name,
     this.date,
@@ -19,16 +23,24 @@ class NotificationModel {
   });
 
   factory NotificationModel.fromJson(Map<String, dynamic> parsedJson) {
+    var keyFromJson = parsedJson['key'];
     var descriptionFromJson = parsedJson['description'];
     var nameFromJson = parsedJson['name'];
     var dateFromJson = parsedJson['date']; // toDate
-    var timeFromJson = parsedJson['time']; //toTime
+    var timeFromJson = parsedJson['time']; // toTime
+
+    var keyList = new List<String>.from(keyFromJson);
+    var descriptionList = new List<String>.from(descriptionFromJson);
+    var nameList = new List<String>.from(nameFromJson);
+    var dateList = new List<DateTime>.from(dateFromJson);
+    var timeList = new List<DateTime>.from(timeFromJson);
 
     return new NotificationModel(
-      description: descriptionFromJson,
-      name: nameFromJson,
-      date: dateFromJson,
-      time: timeFromJson
+      key: keyList,
+      description: descriptionList,
+      name: nameList,
+      date: dateList,
+      time: timeList
     );
   }
 }
--- lib/notification.dart
+++ lib/notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/model/notification_model.dart';
 import 'package:flutter_app/services/notification_service.dart';
 
 class NotificationPage extends StatefulWidget {
@@ -8,7 +9,28 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
-  final notificationList = [ { 'title': 'title 1' }, { 'title': 'title 2' }, { 'title': 'title 3' } ];
+  final notificationList = [
+    {
+      "name": [
+        {"test1"}, {"test2"}, {"test3"},
+      ]
+    },
+    {
+      "description": [
+        {"testDesc1"}, {"testDesc2"}, {"testDesc3"}
+      ]
+    },
+    {
+      "date": [
+        {12456}, {125234}, {3467756}
+      ]
+    },
+    {
+      "time": [
+        {145435}, {425435234}, {14123178}
+      ]
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +49,60 @@ class _NotificationPageState extends State<NotificationPage> {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Center(
+      body: FutureBuilder(
+        future: getNotifications(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text('error' + ' ' + snapshot.data.toString());
+            }else {
+              return Text(snapshot.data.toString());
+            }
+            return new Center (
+              child: ListView.builder(
+                itemCount: snapshot.data.name.length,
+                itemBuilder: (context, i) {
+                  return Card(
+                    key: snapshot.data.key[i],
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: snapshot.data.name[i],
+                          ),
+                          title: Text(snapshot.data.name),
+                          subtitle: Text(snapshot.data.description),
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            RaisedButton(
+                              child: Text('Accept'),
+                              onPressed: () {},
+                            ),
+                            FlatButton(
+                              child: Text('Reject'),
+                              onPressed: () {},
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          }else {
+            return CircularProgressIndicator();
+          }
+        },
+      )
+    );
+  }
+}
+
+
+/*
+Center(
         child: ListView.builder(
           itemCount: notificationList.length,
           itemBuilder: (context, i) {
@@ -53,25 +128,10 @@ class _NotificationPageState extends State<NotificationPage> {
                       )
                     ],
                   ),
-                  FutureBuilder(
-                    future: getNotifications(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        if (snapshot.hasError) {
-                          return Text('error');
-                        }
-                        return Text(snapshot.data.toString());
-                      }else {
-                        return CircularProgressIndicator();
-                      }
-                    },
-                  )
                 ],
               ),
             );
           }
         )
       ),
-    );
-  }
-}
+* */
